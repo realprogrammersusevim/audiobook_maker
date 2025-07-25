838e2f6f99a1c8b77b104c2e93ec37228c38ab04
Jonathan <git@jonathansm.dev>
2025-07-24 21:13:49 -0500

    feat(convert.py): add cli arg parsing

--- convert.py
+++ convert.py
@@ -1,14 +1,20 @@
 import os
+import argparse
 
-os.makedirs("converted", exist_ok=True)
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", default="audio")
+parser.add_argument("-o", "--output", default="converted")
+args = parser.parse_args()
 
-for file in sorted(os.listdir("audio")):
-    source_path = os.path.join("audio", file)
+os.makedirs(args.output, exist_ok=True)
+
+for file in sorted(os.listdir(args.input)):
+    source_path = os.path.join(args.input, file)
     if not os.path.isfile(source_path):
         continue
 
     name, ext = os.path.splitext(file)
-    dest_path = os.path.join("converted", f"{name}.opus")
+    dest_path = os.path.join(args.output, f"{name}.opus")
     if os.path.exists(dest_path):
         continue
 
